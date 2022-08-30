60648e580a5f89105894cd926533994ae59f9e61
Abdulaziz6921 <abdulaziztojibayev6@gmail.com>
2022-08-30 15:00:22 +0500

    firtst push

---
+++ app.js
@@ -0,0 +1,37 @@
+// ozgaruvchilarni nomlovchilar
+
+// let
+// var
+// const
+
+const tugilganKun = 24;
+
+let bitirganKun = 15;
+bitirganKun = 30;
+
+var uylanganKun = 18;
+uylanganKun = 22;
+
+console.log("Meni tugilgan kunim " + tugilganKun + "-yanvar");
+console.log("Bitirgan kunim " + bitirganKun + "-fevral");
+console.log("Uylangan kunim " + uylanganKun + "-may");
+
+// let i = 0;
+// while (i < 3) {
+//   console.log(i, "hello");
+//   i++;
+// }
+
+// let j = 3;
+// while (j) {
+//   console.log(j, "j");
+//   j--;
+// }
+
+// let k = 0;
+// do {
+//   console.log(k, "k");
+//   k++;
+// } while (k < 3);
+// {
+// }
